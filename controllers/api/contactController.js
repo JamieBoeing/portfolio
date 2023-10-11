44f055af087b73cb720731a11a78d7f2a0d0c188
jamie boeing <bleupaon@outlook.com>
2023-10-10 21:28:49 -0500

    controller bug fix

--- controllers/api/contactController.js
+++ controllers/api/contactController.js
@@ -1,33 +1,33 @@
-const sgMail = require('@sendgrid/mail');
-require('dotenv').config()
+// const sgMail = require('@sendgrid/mail');
+// require('dotenv').config()
 
-sgMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
 
 
-const sendEmail = async (req, res) => {
-  try {
-    const { name, email, message } = req.body;
+// const sendEmail = async (req, res) => {
+//   try {
+//     const { name, email, message } = req.body;
 
-    if (!name || !email || !message ) {
-      return res.status(400).json({ message: 'Invalid request body' });
-    }
+//     if (!name || !email || !message ) {
+//       return res.status(400).json({ message: 'Invalid request body' });
+//     }
 
-    const msg = {
-      to: process.env.EMAIL_USER,
-      from: user_email,
-      subject: 'New Contact Form Submission',
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+//     const msg = {
+//       to: process.env.EMAIL_USER,
+//       from: user_email,
+//       subject: 'New Contact Form Submission',
+//       text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
 
-    };
+//     };
 
-    await sgMail.send(msg); 
-    return res.status(200).json({ message: 'Email sent successfully' });
-  } catch (error) {
-    console.error('Error sending email:', error);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-};
+//     await sgMail.send(msg); 
+//     return res.status(200).json({ message: 'Email sent successfully' });
+//   } catch (error) {
+//     console.error('Error sending email:', error);
+//     return res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
-module.exports = {
-  sendEmail,
-};
+// module.exports = {
+//   sendEmail,
+// };
